d918932f80537028e2c765e4f9307b40edbc32fe
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-06-15 11:05:30 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,19 @@
 /*Created 2015-06-12  by Rajbir Karan Singh*/
+
+function init(e){
+    $('#open_search').click(function(){
+        $('#open_search').hide()
+        $('#close_search').css('display','inline-block');
+        $('#search').show('slow')
+        $('#search').focus()
+        e.preventDefault()
+    })
+
+    $('#close_search').click(function(){
+        $('#close_search').hide()
+        $('#open_search').css('display','inline-block');
+        $('#search').hide('slow')
+        e.preventDefault()
+    })
+
+}
