7687388d91987453898cd19eed6e57f3d63ff177
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-08 10:42:22 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -1,5 +1,11 @@
 /*Created 2015-06-12  by Rajbir Karan Singh*/
 
+//browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 300,
+	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+	offset_opacity = 1200,
+	//duration of the top scrolling animation (in ms)
+	scroll_top_duration = 700;
 function init(e){
     $('#open_search').click(function(){
         $('#open_search').hide();
