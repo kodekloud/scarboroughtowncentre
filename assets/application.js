337e8d29d1c03bbccc88f86f4207b908c56eaf50
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-09 13:11:27 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -191,7 +191,7 @@ function getImageResolution(url){
     $("<img/>").attr("src", url).load(function(){
         s['width'] = this.width;
         s['height']' = this.height;
-        console.log(this.width)
+       
         }); 
     return s;
 }
