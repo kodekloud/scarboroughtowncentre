5ccc16ee28fd9ce7d3101b0c1736bf03e0484614
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-09 15:44:19 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -172,12 +172,7 @@ function renderStoreDetails(container, template, collection, slug){
         val.map_x_coordinate = val.x_coordinate - 19;
         val.map_y_coordinate = val.y_coordinate - 58;
         
-        if (val.website.length > 0){
-            val.show = "display:block";
-        }
-        else{
-            val.show = "display:none";
-        }
+        
         val.todays_hours = todays_hours
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
