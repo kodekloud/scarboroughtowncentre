916da98584770935e6cde1b302f20885c1592714
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-09 17:37:24 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -223,6 +223,7 @@ function renderStoreExtras(container, template, type, ids){
         } else {
             val.dates = (get_month(start.getMonth()))+" "+(start.getDate())+" - "+get_month(end.getMonth())+" "+end.getDate();    
         }
+        val.closing_date = (get_month(start.getMonth()))+" "+(start.getDate());  
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
