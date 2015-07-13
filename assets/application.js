96e7ab6a62cb3cc8f662c7097ccc3c98ce283eb2
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-13 14:44:12 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -252,10 +252,10 @@ function renderGeneral(container, template, collection, type){
             if (val.jobable_type == "Store") {
                 var store_details = getStoreDetailsByID(val.jobable_id);
                 if ((store_details.store_front_url_abs).indexOf('missing.png') > -1) {
-                        val.alt_promo_image_url = "http://assets.kodekloud.io/sites/557af89f6e6f64717a000000/3dbb78c8bf8493b2de511c175b2a425b/stc_logo.png"
-                    } else {
-                        val.alt_promo_image_url = (store_details.store_front_url_abs);    
-                    }
+                    val.alt_promo_image_url = "http://assets.kodekloud.io/sites/557af89f6e6f64717a000000/3dbb78c8bf8493b2de511c175b2a425b/stc_logo.png"
+                } else {
+                    val.alt_promo_image_url = (store_details.store_front_url_abs);    
+                }
                 val.store_name = store_details.name;
             }
             else{
