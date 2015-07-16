5b8941f12e9889555565c1ea319146efbe0c0763
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-16 14:17:37 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -71,10 +71,10 @@ function init_home_hours(){
     Mustache.parse(template_html);
     $.each(hours_today, function(key, val){
        val.open = val.open_time;
+       console.log(val)
        var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
-    console.log(item_rendered)
     $('#home_hours_container').show();
     $('#home_hours_container').html(item_rendered.join(''));
 }
