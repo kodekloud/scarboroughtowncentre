626edc9c3c765b8371ab3dbd43f138cd6b56015b
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-16 14:43:06 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -84,7 +84,7 @@ function init_home_hours(){
 }
 function check_open_time(d){
     var date = new Date();
-    var current_time = d.getUTCHours();
+    var current_time = date.getUTCHours();
     console.log(current_time)
     console.log(d.getUTCHours())    
 }
