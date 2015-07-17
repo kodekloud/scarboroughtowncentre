73e529a69b42d984f5fe0c050168f6fe93e986ed
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-17 12:48:33 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -74,36 +74,6 @@ function init_home_hours(){
     });
     $('#home_hours_container').html(item_rendered.join(''));
 }
-function check_open_time(d){
-    var time = "";
-    var date = new Date();
-    var current_time = date.getHours();
-    var open = d.getUTCHours();
-    if (current_time >= open ){
-        time = "OPEN NOW";
-    }
-    else{
-        time = convert_hour(d);
-    }
-    return time;
-}
-function more_less(e){
-    $('.more').click(function(e){
-        var id = $(this).attr('data-id');
-        $(this).hide();
-        $('#show_' + id).show();
-        $('#less_' + id).show();
-        e.preventDefault();
-    });
-    
-    $('.less').click(function(e){
-        var id = $(this).attr('data-id');
-        $(this).hide();
-        $('#show_' + id).hide();
-        $('#more_' + id).show();
-        e.preventDefault();
-    });
-}
 
 function renderStoreList(container, template, collection, type,starter, breaker){
     var item_list = [];
