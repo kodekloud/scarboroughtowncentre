b8d5e92033c656764c1d528241344883ff75510d
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-17 12:52:19 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -631,7 +631,7 @@ function renderPostDetails(container, template, collection){
     var item_list = [];
     var item_rendered = [];
     var template_html = $(template).html();
-    item_list.push(collection)
+    item_list.push(collection);
     $.each( collection , function( key, val ) {
         if (val.image_url.indexOf('missing.png') > -1) {
             val.post_image = "http://assets.kodekloud.io/sites/557af89f6e6f64717a000000/3dbb78c8bf8493b2de511c175b2a425b/stc_logo.png";
