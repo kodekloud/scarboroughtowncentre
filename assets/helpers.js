5bac45da65fc14a44c281496836775fe105c5f8c
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-17 16:02:11 -0400

    via kodekloud.io (55a922896e6f6419b7000000)

--- assets/helpers.js
+++ assets/helpers.js
@@ -141,15 +141,15 @@ function load_more(num){
 
 
 $('#search_input').keyup(function(e){
-    showSearchResults();
+    showSearchResults(e);
 });
 
-$('#search_input').on('input', function() {
-    showSearchResults();
+$('#search_input').on('input', function(e) {
+    showSearchResults(e);
 });
             
             
-function showSearchResults(){
+function showSearchResults(e){
     $('#search_results').show();
     if($('#search_input').val().length == 0){
         $('#search_results').hide();
