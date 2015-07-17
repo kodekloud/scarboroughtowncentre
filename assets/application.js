0cee222e545c48db10c8f3943b12e150442a7803
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-17 13:43:49 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -647,7 +647,7 @@ function renderPostDetails(container, template, collection){
         val.published_on = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
         var next_p = getNextPublishedPostBySlug(val.slug);
         var prev_p = getPrevPublishedPostBySlug(val.slug);
-        if (next_p == 'undefined'){
+        if (next_p == undefined){
             val.next_post_show = "display:none";
         }
         else{
