6c235c2833386b99bc7d0dbf3d7fda75748f137a
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-17 13:38:53 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -645,7 +645,6 @@ function renderPostDetails(container, template, collection){
         }
         var date_blog = new Date(val.publish_date);
         val.published_on = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
-        console.log(val.slug)
         var next_p = getNextPublishedPostBySlug(val.slug)
         var prev_p = getPrevPublishedPostBySlug(val.slug)
         
