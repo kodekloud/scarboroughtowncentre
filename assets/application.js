ec87439ce19761b58014c6750495de20d77c3ce6
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-17 11:56:40 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -762,6 +762,6 @@ function renderPosts(container, template, collection){
 function load_more(num){
     for(i=num; i < num+2; i++){
         var id = i.toString();
-        $('#show_' + id ).show()
+        $('#show_' + id ).show();
     }
 }
