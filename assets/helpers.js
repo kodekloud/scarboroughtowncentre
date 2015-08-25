252b50b42e2596d51a919491a8ff5842ccba4b88
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-25 11:17:47 -0400

    via kodekloud.io (55a922896e6f6419b7000000)

--- assets/helpers.js
+++ assets/helpers.js
@@ -4,6 +4,16 @@ function show_content(){
     
 }
 
+function pinIt(){
+    var e = document.createElement('script');
+    e.setAttribute('type','text/javascript');
+    e.setAttribute('charset','UTF-8');
+    e.setAttribute('src','https://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);
+    document.body.appendChild(e);
+    return false;
+}
+
+
 function check_open_time(d){
     var time = "";
     var date = new Date();
