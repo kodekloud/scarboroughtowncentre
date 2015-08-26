4e4615159eb9ba53328cde683f2ac7ea1028141d
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-26 11:16:01 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -670,6 +670,7 @@ function renderPosts(container, template, collection){
         if(result.length > 0){
             val.slug = val.video_link;
         }
+        console.log(val.video_link)
         val.counter = counter;
         var date_blog = new Date((val.publish_date + " 05:00:00").replace(/-/g,"/"));
         val.published_on = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
