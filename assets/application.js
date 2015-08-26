70765e2479f6b3e4a079c87be0c0530cd515fb72
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-26 11:13:52 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -663,6 +663,7 @@ function renderPosts(container, template, collection){
         }
         val.slug = "trending/" +val.slug;
         var lb = getBlogDataBySlug("stc-lookbook");
+        var contest = getBlogDataBySlug("stc-lookbook");
         var id = val.id;
         var result = $.grep(lb.posts, function(e){ return e.id == id; });
         if(result.length > 0){
