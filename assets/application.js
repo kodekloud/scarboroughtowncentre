0d7b63122ba5676891118b2c9fe73bc59d9350e3
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-26 11:31:04 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -115,7 +115,7 @@ function init_home_hours(){
                 item_rendered.push(rendered);
             });
         } 
-        else{
+        else(val.day_of_week == n && val.is_closed == false && val.is_holiday == false){
             val = {};
             val.open = "closed";
             val.close = "closed";
