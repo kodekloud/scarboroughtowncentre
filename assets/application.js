7776e5ef2eb944df590a7c60e4b65038d1ea3b15
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-28 15:22:53 -0400

    via kodekloud.io (557af89f6e6f64717a050000)

--- assets/application.js
+++ assets/application.js
@@ -718,39 +718,6 @@ function renderPosts2(container, template, collection){
 }
 
 
-function renderPosts22(container, template, collection){
-    var item_list = [];
-    var item_rendered = [];
-    var template_html = $(template).html();
-    var counter = 1;
-    console.log(collection)
-    Mustache.parse(template_html);   // optional, speeds up future uses
-    $.each( collection , function( key, val ) {
-        if (val.image_url.indexOf('missing.png') > -1) {
-            val.post_image = "http://assets.kodekloud.io/sites/557af89f6e6f64717a000000/8145457936ef8cb613a266a0fefedb69/STC%20Newsletter.jpg";
-        } else {
-            val.post_image = val.image_url;
-        }
-        if(val.body.length > 100){
-            val.description_short = val.body.substring(0,100) + "...";
-        }
-        else{
-            val.description_short = val.body;
-        }
-        
-        
-        val.counter = counter;
-        var date_blog = new Date((val.publish_date + " 05:00:00").replace(/-/g,"/"));
-        val.published_on = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
-        
-        var rendered = Mustache.render(template_html,val);
-        item_rendered.push(rendered);
-        counter = counter+1;
-    });
-    
-    $(container).show();
-    $(container).html(item_rendered.join(''));
-}
 
 function renderPostDetails(container, template, collection){
     var item_list = [];
