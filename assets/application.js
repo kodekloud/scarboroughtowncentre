fb13c554d8330f5d5302c34b86bb2798929e1f43
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-09-03 13:06:34 -0400

    via kodekloud.io (55e7179b6e6f6451630c0000)

--- assets/application.js
+++ assets/application.js
@@ -804,23 +804,23 @@ function renderFeatureItems(feature_template,feature_items,featureList){
 }
 
 function renderPageData(container, template, collection, type){
-            var item_list = [];
-            var item_rendered = [];
-            var template_html = $(template).html();
-            Mustache.parse(template_html);   // optional, speeds up future uses
-            $.each( collection , function( key, val ) {
-                
-                var title = val.title.split(" - ");
-                val.title = title[1]
-                
-                var rendered = Mustache.render(template_html,val);
-                item_rendered.push(rendered);
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    $.each( collection , function( key, val ) {
+        
+        var title = val.title.split(" - ");
+        val.title = title[1]
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+
+    });
     
-            });
-            
-            $(container).show();
-            $(container).html(item_rendered.join(''));
-        };
+    $(container).show();
+    $(container).html(item_rendered.join(''));
+};
 
 
 
