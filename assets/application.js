7de7a36b37ed2c595b31a81876c5bd82fabe1a4c
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-09-03 13:06:41 -0400

    via kodekloud.io (55e7179b6e6f6451630c0000)

--- assets/application.js
+++ assets/application.js
@@ -803,7 +803,7 @@ function renderFeatureItems(feature_template,feature_items,featureList){
     
 }
 
-function renderInsidePages(container, template, collection, type){
+function renderInsidePages(container, template, collection){
     var item_list = [];
     var item_rendered = [];
     var template_html = $(template).html();
